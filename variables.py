ca4cb5600bf93535a86b36705d0f92ca18b502c8
Md Shohanuzzaman <49681825+quietsos@users.noreply.github.com>
2020-02-07 12:31:41 +0600

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,19 @@
+number1 = 5
+print(number1)
+number1 = 3
+print(number1)
+
+# variable can store any types of data like string, integer, float, double etc
+
+name = "Md Shohanuzzaman"
+print(name)
+
+# Rules of declaration a variable 
+
+# rule 1: we cannot start variable name using numeric number ex: 1number (not vaild variable)
+# rule 2: we can start variable using (Letters,_ )
+# rule 3: we cannot user any special symbole ($,@) start,within or end of any variable declaration
+
+user_one_name = "Shohan" #This type of variable(using "_") declaration is called snake cased writting.
+userOneName = "Shohan"  #This type of variable(first word start small letter then after each word start with capital letter) is called camel case writting
+print(user_one_name,userOneName)
