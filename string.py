8a9ba26c33f2ed8940f6c3b49b43f818160bb46e
Md Shohanuzzaman <49681825+quietsos@users.noreply.github.com>
2020-02-07 22:25:51 +0600

    Add files via upload

---
+++ string.py
@@ -0,0 +1,24 @@
+#strings 
+# String is the collection of character inside single or double quotes.
+
+first_name = "Md"
+last_name = "Shohanuzzaman"
+
+# String concatation
+
+full_name = first_name + " " +last_name
+print(full_name)
+
+# We cannot add a numeric number with a string directely.
+
+# print(first_name + 5) # it will show type error
+
+# we can add number with a string two ways
+
+print(first_name + "5") #using double cotesion
+print(first_name + str(5)) #using string function
+
+# we can use multiply (*) for repeat string 
+
+print(first_name * 3)
+
