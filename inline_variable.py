6d6827daa86ffff1ef1e699442738104a29358a2
Md Shohanuzzaman <49681825+quietsos@users.noreply.github.com>
2020-02-07 22:40:19 +0600

    Add files via upload

---
+++ inline_variable.py
@@ -0,0 +1,11 @@
+#Declaring multiple variable in a line
+
+name, age, cgpa = "Shohan", 21, 3.50
+print("Your name is: " + name + " "+ "Age: "+ str(age) + " " + "Cgpa: "+ str(cgpa))
+
+# Similarly in a line we can declare same value in various variable.
+
+x=y=z= 1
+
+print("x= "+ str(x)+ " y= "+str(y)+ " z= "+str(z))
+print(x+y+z)
