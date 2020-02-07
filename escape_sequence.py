2d1939e373c5c41667f5e0721b09eebfb3e1198e
Md Shohanuzzaman <49681825+quietsos@users.noreply.github.com>
2020-02-07 12:30:46 +0600

    Add files via upload

---
+++ escape_sequence.py
@@ -0,0 +1,26 @@
+print("Hello 'hello' world")
+print("Hello \"hello world")
+print("Hello hello\" world")
+print("Hello \"hello\" world")
+print("Line A Line B")
+print("Line A \nLine B")
+print("Hello \ world")
+print("Hello world\\")
+print("Hello \'world\'")
+print("Hello \nworld") # \n used for new line(short cart comment line creation is controle+forward slash)
+print("Hello \tworld")
+print("Hello \bworld") #\b is used for backspace
+print("Hello \\n world") # \\n for printing \n in inline
+print("Hello \\t world") #\\t for printing \t in inline
+print("hello world \\\\") # \ backslash printing...for output backslash insert double number \ in input
+print(" \\\" \\\'")
+
+#Exercise problem solving
+print("this is \\\\ double bakcslash") #printing double backslash
+print("these are /\\/\\/\\/\\/\\")   #/\/\/\/\/\ printing this sequence using escape sequecnce
+print("he is \t awesome") # printing tap space within line
+print("\\\" \\n \\t \\'") #printing \" \n \t \' sequence
+
+
+# Excape sequence using as normal text
+print(r"Hello \n world") #using small r outer start of double cotation in print function we can ignor escape sequence
