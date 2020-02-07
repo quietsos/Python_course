8ac511f1396784d02dad571266cf75d9ea8a2fcf
Md Shohanuzzaman <49681825+quietsos@users.noreply.github.com>
2020-02-07 12:31:22 +0600

    Add files via upload

---
+++ python_as_calculator.py
@@ -0,0 +1,18 @@
+print(3+7) #addition operator
+print(3*6) #multiplication operator
+print(5-9) #substration operator
+print(5/2) #float division
+print(5//2) #integer division
+print(5%2) #modulus 
+print(3**3) #exponential calculation
+print(9**.5) #square root calculation usingi exponential calculation
+
+
+#round function 
+
+print(round(5**.5,4)) #round function is used for select the digit after decimal point 
+
+# precedence and associative rules are applied for calcultaing a large mathmatical equation
+# precedence rule: parenthecis, exponential, (*,/,//,%),(+,-)
+# Here *,/,//,% are associative from left to right in calculation 
+# For exponential ** associative rule work from right to left.
