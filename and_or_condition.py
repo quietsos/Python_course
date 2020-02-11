248d9107473441118116f870552d6dcd05f756d7
Md Shohanuzzaman <49681825+quietsos@users.noreply.github.com>
2020-02-11 11:07:10 +0600

    Add files via upload

---
+++ and_or_condition.py
@@ -0,0 +1,23 @@
+
+# Conditional operator and/or
+
+# //and// condition is being true when both side of the condition is being true 
+
+name = input("Enter your name:")
+age = int(input("Enter your age:"))
+
+if name == "sohan" and age == 21:
+    print("Condition true.")
+
+else:
+    print("Condition false")
+    
+
+# //or// condition is being work when one of the two condition is true or both condition is true
+
+if name == "sohan" or age == 21:
+    print("Condition true.")
+
+else:
+    print("Condition false.")
+         
