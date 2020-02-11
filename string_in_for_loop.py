e8fbbd9cf2a1fd21146d9cf254ebbbded6a1b8d9
Md Shohanuzzaman <49681825+quietsos@users.noreply.github.com>
2020-02-11 16:44:34 +0600

    Add files via upload

---
+++ string_in_for_loop.py
@@ -0,0 +1,13 @@
+
+#conventional uses of for loop in printing character of any string.
+
+name = "shohan"
+for i in range(len(name)):
+    print(name[i])
+
+
+# string and for loop in python 
+
+name = "shohan"
+for i in name:
+    print(i)
