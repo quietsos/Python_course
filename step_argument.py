7d0a73291c99157de1da65af59ebcda6dae13b59
Md Shohanuzzaman <49681825+quietsos@users.noreply.github.com>
2020-02-11 16:38:02 +0600

    Add files via upload

---
+++ step_argument.py
@@ -0,0 +1,21 @@
+# step argument in python is defined in the range() function of for loop
+
+# syntax - for variable in range(start,end,step)
+       #here step define how many step will overlapping for reading the next number
+
+
+#printing odd number using step arguments
+
+for i in range(1,10,2):
+    print(i)
+
+#printing even number using step arguments
+
+for i in range(2,10,2):
+    print(i)
+
+
+# Reverse number print using step arguments
+
+for i in range(10, 0, -1):
+    print(i)
