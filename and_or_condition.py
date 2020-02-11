2a4cf71a5c62112e8d7512e48e231d394f34e2d5
Md Shohanuzzaman <49681825+quietsos@users.noreply.github.com>
2020-02-11 11:22:16 +0600

    Add files via upload

--- and_or_condition.py
+++ and_or_condition.py
@@ -1,23 +1,38 @@
 
-# Conditional operator and/or
+# # Conditional operator and/or
 
-# //and// condition is being true when both side of the condition is being true 
+# # //and// condition is being true when both side of the condition is being true 
 
-name = input("Enter your name:")
-age = int(input("Enter your age:"))
+# name = input("Enter your name:")
+# age = int(input("Enter your age:"))
 
-if name == "sohan" and age == 21:
-    print("Condition true.")
+# if name == "sohan" and age == 21:
+#     print("Condition true.")
 
-else:
-    print("Condition false")
+# else:
+#     print("Condition false")
     
 
-# //or// condition is being work when one of the two condition is true or both condition is true
+# # //or// condition is being work when one of the two condition is true or both condition is true
 
-if name == "sohan" or age == 21:
-    print("Condition true.")
+# if name == "sohan" or age == 21:
+#     print("Condition true.")
+
+# else:
+#     print("Condition false.")
+         
+
+                #EXERCISE
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+start_string = name[0]
+print(start_string)
+
+if (start_string == 'a' or start_string=='A') and age > 10:
+    print("you can watch coco movie.")
 
 else:
-    print("Condition false.")
-         
+    print("you cannot watch cooc movie.")
+    
