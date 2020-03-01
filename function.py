83196de704616c82738029466397a2aa80dfc255
Md Shohanuzzaman <49681825+quietsos@users.noreply.github.com>
2020-03-01 23:40:22 +0600

    Add files via upload

---
+++ function.py
@@ -0,0 +1,41 @@
+# # This is a function defination for adding two integer number
+# def add(num1,num2):
+#     return(num1+num2)
+
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
+
+# sum = add(num1,num2)
+# print(sum)
+
+# # This funciton defination is for adding two string with each other or string concatetion
+
+# def add(num1,num2):
+#     return(num1+num2)
+
+# num1 = input("Enter first number: ")
+# num2 = input("Enter second number: ")
+
+# sum = add(num1,num2)
+# print(sum)
+
+
+
+                    # Function practice
+
+
+# def last_char(name):
+#     return(name[-1])
+
+# print(last_char(input("Enter your name:")))
+ 
+            # Practice even odd function
+
+
+def even_odd(num):
+    if num%2 == 0:
+        return "Even"
+    else:
+        return "odd"
+
+print(even_odd(int(input("Enter a number:"))))
