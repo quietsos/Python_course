e1e7a276bbdabc9e6682aa96155a2b638ed3ab33
Md Shohanuzzaman <shohaniu8@gmail.com>
2020-03-31 14:56:32 +0600

    Add files via upload

---
+++ file_handing/file_handling.py
@@ -0,0 +1,32 @@
+# File handling is refers to handle file in python.
+# Basically file handling operate    CRUD operation:
+#     CRUD:
+#          1. creating file
+#          2. read file
+#          3. updating file
+#          4. deleting file
+
+# The key function for working with files in python is the open() function.
+
+#     cycle is:
+#             1. create file
+#             2. open file 
+#             3. work on file
+#             4. close file then as follow
+
+#     function:    open(filename,mode) 
+#                     1.file name ( any name of the file that want to work on)
+#                     2. mode( mode is the purpose of the working on file)
+#                         1. "r"=Reading file
+#                         2. "w"=writting file
+#                         3. "a"=appending file
+#                         4. "x"=creating file
+#                         5. "t" = text
+#                         6. "b"=binary mode
+
+
+# Read operation:
+    # syntax: file.read()
+    # example: file = open("testfile.text"."r")
+    #               = print file.read()
+
