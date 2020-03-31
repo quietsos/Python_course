27cf1864d1b3eb4ee4940a141006bb9531d7d6e3
Md Shohanuzzaman <shohaniu8@gmail.com>
2020-03-31 14:59:09 +0600

    Add files via upload

--- file_handing/file_handling_exercise.py
+++ file_handing/file_handling_exercise.py
@@ -4,38 +4,38 @@
 
 # example 1:
 
-# import os
-# file = open("/home/quiet/Desktop/python/file_handing/file_handling_exercise.txt", "w")
-# file.close()
+import os
+file = open("/home/quiet/Desktop/python/file_handing/file_handling_exercise.txt", "w")
+file.close()
 
 #example 2: reading document text file using python code.
 
-# import os
-# file = open("/home/quiet/Desktop/python/file_handing/file_handling_exercise.txt", 'r')
-# print(file.read())
-# file.close()
+import os
+file = open("/home/quiet/Desktop/python/file_handing/file_handling_exercise.txt", 'r')
+print(file.read())
+file.close()
 
 # example 3: Readding the exact number of string using the python code of any text file
 
-# import os
-# file = open("/home/quiet/Desktop/python/file_handing/file_handling_exercise.txt", 'r')
-# print(file.read(5))
-# file.close()
+import os
+file = open("/home/quiet/Desktop/python/file_handing/file_handling_exercise.txt", 'r')
+print(file.read(5))
+file.close()
 
 # exercise 5: read all the line on a text file using python code.
 
-# import os
-# file = open("/home/quiet/Desktop/python/file_handing/file_handling_exercise.txt", 'r')
-# print(file.readlines())
-# file.close()
+import os
+file = open("/home/quiet/Desktop/python/file_handing/file_handling_exercise.txt", 'r')
+print(file.readlines())
+file.close()
 
 # exercise 6: read all the line on a text file using for loop python code 
 
-# import os
-# file = open("/home/quiet/Desktop/python/file_handing/file_handling_exercise.txt", 'r')
-# for line in file:
-#     print(file.readlines())
-# file.close()
+import os
+file = open("/home/quiet/Desktop/python/file_handing/file_handling_exercise.txt", 'r')
+for line in file:
+    print(file.readlines())
+file.close()
 
 
 # Writting method in a text file using python:
@@ -46,41 +46,43 @@
     
 
 # exercis 1: checking that existing file is really writeable or not 
-# import os
-# file = open("/home/quiet/Desktop/python/file_handing/file_handling_exercise.txt", 'w')
-# print(file.writable())
-# file.close()
+
+import os
+file = open("/home/quiet/Desktop/python/file_handing/file_handling_exercise.txt", 'w')
+print(file.writable())
+file.close()
 
 # exercise 2: writting text on a text file using python 
-# import os
-# file = open("/home/quiet/Desktop/python/file_handing/file_handling_exercise.txt", 'w')
-# file.write("Hello world!\n")
-# file.write("love for python!")
-# file.close()
+
+import os
+file = open("/home/quiet/Desktop/python/file_handing/file_handling_exercise.txt", 'w')
+file.write("Hello world!\n")
+file.write("love for python!")
+file.close()
 
 #exercise 3: checking is "w" working on python
 
-# import os
-# file = open("/home/quiet/Desktop/python/file_handing/file_handling_exercise.txt", 'w')
-# file.write("Oops overwritting!\n")
-# file.close()
+import os
+file = open("/home/quiet/Desktop/python/file_handing/file_handling_exercise.txt", 'w')
+file.write("Oops overwritting!\n")
+file.close()
 
 #exercise 4: checking "a" mode is working on python
 
-# import os
-# file = open("/home/quiet/Desktop/python/file_handing/file_handling_exercise.txt", 'a')
-# file.write("append is working.\n")
-# file.close()
+import os
+file = open("/home/quiet/Desktop/python/file_handing/file_handling_exercise.txt", 'a')
+file.write("append is working.\n")
+file.close()
 
 
 
 # Creating file using pyton:
     # "x" mode is used for creating file.
 
-# import os
-# file = open("/home/quiet/Desktop/python/file_handing/new_file.txt", 'x')
-# file.write("New file is created.\n")
-# file.close()
+import os
+file = open("/home/quiet/Desktop/python/file_handing/new_file.txt", 'x')
+file.write("New file is created.\n")
+file.close()
 
 # Deleting file using python:
 
@@ -89,8 +91,8 @@
 
 # exercise 1:
 
-# import os
-# os.remove("/home/quiet/Desktop/python/file_handing/delete_file.txt")
+import os
+os.remove("/home/quiet/Desktop/python/file_handing/delete_file.txt")
 
 # exercise 2:
 
