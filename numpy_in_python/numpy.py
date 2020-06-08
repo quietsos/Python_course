1e1b2053b78c20096b9af50a6a660375b4310574
Md Shohanuzzaman <shohaniu8@gmail.com>
2020-06-08 16:45:02 +0600

    Add files via upload

---
+++ numpy_in_python/numpy.py
@@ -0,0 +1,8 @@
+# Numpy:
+#     Numpy is the core library for scientific computing in python.
+#     It provides a high performance multidimensional array object, and tools for working with these array.
+
+import numpy as np 
+
+a = np.array([1,2,3,4])
+print(a) 
